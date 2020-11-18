0dc74f4beab85141e455649dcbfcf7110f6da6a2
johnmogi <anguru@gmail.com>
2020-11-18 20:01:21 +0200

    a

---
+++ 1.js
@@ -0,0 +1,35 @@
+
+// var x = "hi shaked";
+
+// function hello(){
+//   var name =  prompt("what's your name? ")
+ 
+      
+//       alert("hi, " + name);
+ 
+// }
+
+// hello();
+
+
+var i = 0;
+
+let j =0;
+
+i = 1
+
+console.log(
+
+    "j: " +  j
+)
+
+console.log(
+    "i: " + i 
+)
+
+
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
